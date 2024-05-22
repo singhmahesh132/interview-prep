85ee80d822a27bd5f1ecb7133f386f10524b2e7e
Mahesh Singh <159167042+singhmahesh132@users.noreply.github.com>
2024-05-22 13:28:24 +0530

    Update FreqSortUsingStreams.java

--- src/Programs/FreqSortUsingStreams.java
+++ src/Programs/FreqSortUsingStreams.java
@@ -10,8 +10,11 @@ public class    FreqSortUsingStreams {
     public static void main(String[] args){
         Integer[] arr = {1,1,1,2,2,2,2,3,3,3,4,5,5};
         List<Integer> arrToList = Arrays.stream(arr).toList();
+
         Map<Integer,Integer> freqMap = new HashMap<>();
+
         arrToList.forEach(num -> freqMap.put(num, freqMap.getOrDefault(num,0)+1));
+
         freqMap.entrySet().stream().sorted((o1, o2) ->
                 o2.getValue().compareTo(o1.getValue()) == 0 ? o2.getKey().compareTo(o1.getKey()) : o2.getValue().compareTo(o1.getValue())
         ).forEach(entry -> System.out.println(entry.getKey()));
