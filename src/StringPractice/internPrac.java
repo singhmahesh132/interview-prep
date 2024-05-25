6d73e070f2b874a31de838c0b2eac37cac97a30f
Mahesh Singh <159167042+singhmahesh132@users.noreply.github.com>
2024-05-25 13:42:28 +0530

    Update internPrac.java

--- src/StringPractice/internPrac.java
+++ src/StringPractice/internPrac.java
@@ -6,10 +6,15 @@ public class internPrac {
 
         String s1 = "hello";
         String s2 = new String("hello");
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s1.hashCode() == s2.hashCode());
         System.out.println(s1==s2);
-
+        
         s2 = s2.intern();
-
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s1.hashCode() == s2.hashCode());
         System.out.println(s1==s2);
     }
 }
